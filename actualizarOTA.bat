66326ff6d84d4a2fc00f3f18dfce2d2cf7aaa20e
rcarreror <rcarreror@gmail.com>
2021-10-15 18:56:17 +0200

    1.3

--- actualizarOTA.bat
+++ actualizarOTA.bat
@@ -1,7 +1,8 @@
 cls 
 if not exist "D:\Users\Roberto\Desktop\UPM\"1er Cuatrimestre"\Cuarto\SBC\Arduino\ESP32_AutoUpdateOTA\esp32_github_ota\esp32_github_ota.ino.esp32.bin"(
 	echo No se encontró el programa compilado exportado. Ve a Arduino, Programa, Exportar, Exportar Binarios compilados.
-) else(
+) 
+if exist "D:\Users\Roberto\Desktop\UPM\"1er Cuatrimestre"\Cuarto\SBC\Arduino\ESP32_AutoUpdateOTA\esp32_github_ota\esp32_github_ota.ino.esp32.bin"(
 	del esp32_github_ota\fw.bin
 	ren esp32_github_ota\esp32_github_ota.ino.esp32.bin fw.bin
 	echo %1 > esp32_github_ota\bin_version.txt
