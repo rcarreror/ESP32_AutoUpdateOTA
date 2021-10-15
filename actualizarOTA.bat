354ba8592885cd0178d5ec6968aa7365b099bbfd
rcarreror <rcarreror@gmail.com>
2021-10-15 18:47:26 +0200

    1.3

--- actualizarOTA.bat
+++ actualizarOTA.bat
@@ -1,6 +1,11 @@
-del esp32_github_ota\fw.bin
-ren esp32_github_ota\esp32_github_ota.ino.esp32.bin fw.bin
-echo %1 > esp32_github_ota\bin_version.txt
-git add .
-git commit -m %1
-git push
+cls 
+if exist "D:\Users\Roberto\Desktop\UPM\"1er Cuatrimestre"\Cuarto\SBC\Arduino\ESP32_AutoUpdateOTA\esp32_github_otaesp32_github_ota.ino.esp32.bin"(
+	del esp32_github_ota\fw.bin
+	ren esp32_github_ota\esp32_github_ota.ino.esp32.bin fw.bin
+	echo %1 > esp32_github_ota\bin_version.txt
+	git add .
+	git commit -m %1
+	git push
+) else(
+	echo No se encontró el programa compilado exportado. Ve a Arduino, Programa, Exportar, Exportar Binarios compilados.
+)
