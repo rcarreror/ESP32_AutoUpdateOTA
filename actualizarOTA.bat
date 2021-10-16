1c1852982475b6919a9410feb3c96229a6d25cd9
rcarreror <rcarreror@gmail.com>
2021-10-16 21:04:55 +0200

    V1.1

--- actualizarOTA.bat
+++ actualizarOTA.bat
@@ -1,12 +1,7 @@
-cls 
-if not exist "D:\Users\Roberto\Desktop\UPM\"1er Cuatrimestre"\Cuarto\SBC\Arduino\ESP32_AutoUpdateOTA\esp32_github_ota\esp32_github_ota.ino.esp32.bin"(
-	echo No se encontró el programa compilado exportado. Ve a Arduino, Programa, Exportar, Exportar Binarios compilados.
-) 
-if exist "D:\Users\Roberto\Desktop\UPM\"1er Cuatrimestre"\Cuarto\SBC\Arduino\ESP32_AutoUpdateOTA\esp32_github_ota\esp32_github_ota.ino.esp32.bin"(
-	del esp32_github_ota\fw.bin
-	ren esp32_github_ota\esp32_github_ota.ino.esp32.bin fw.bin
-	echo %1 > esp32_github_ota\bin_version.txt
-	git add .
-	git commit -m %1
-	git push
-)
+del esp32_github_ota\fw.bin
+ren esp32_github_ota\esp32_github_ota.ino.esp32.bin fw.bin
+echo %1 > esp32_github_ota\bin_version.txt
+git add .
+git commit -m %1
+git push
+	
--- esp32_github_ota/esp32_github_ota.ino
+++ esp32_github_ota/esp32_github_ota.ino
@@ -1,4 +1,4 @@
-#include <WiFi.h>
+  #include <WiFi.h>
 #include <HTTPClient.h>
 #include <HTTPUpdate.h>
 #include <WiFiClientSecure.h>
@@ -9,7 +9,7 @@ const char * password = "helloworld458";
 
 
 String FirmwareVer = {
-  "1.2"
+  "1.1"
 };
 #define URL_fw_Version   "https://raw.githubusercontent.com/rcarreror/ESP32_AutoUpdateOTA/main/esp32_github_ota/bin_version.txt"
 #define URL_fw_Bin           "https://raw.githubusercontent.com/rcarreror/ESP32_AutoUpdateOTA/main/esp32_github_ota/fw.bin"
@@ -29,7 +29,7 @@ const long interval = 60000;
 const long mini_interval = 1000;
 
 void repeatedCall() {
-  static int num=0;
+  static int num = 0;
   unsigned long currentMillis = millis();
   if ((currentMillis - previousMillis) >= interval) {
     // save the last time you blinked the LED
@@ -44,14 +44,14 @@ void repeatedCall() {
     Serial.print(num++);
     Serial.print(" Active fw version:");
     Serial.println(FirmwareVer);
-   if(WiFi.status() == WL_CONNECTED) 
-   {
-       Serial.println("wifi connected");
-   }
-   else
-   {
-    connect_wifi();
-   }
+    if (WiFi.status() == WL_CONNECTED)
+    {
+      Serial.println("wifi connected");
+    }
+    else
+    {
+      connect_wifi();
+    }
   }
 }
 
@@ -60,6 +60,7 @@ struct Button {
   uint32_t numberKeyPresses;
   bool pressed;
 };
+
 
 Button button_boot = {
   0,
@@ -70,7 +71,7 @@ Button button_boot = {
     Button* s = static_cast<Button*>(arg);
     s->numberKeyPresses += 1;
     s->pressed = true;
-}*/
+  }*/
 
 void IRAM_ATTR isr() {
   button_boot.numberKeyPresses += 1;
@@ -88,13 +89,14 @@ void setup() {
   connect_wifi();
 }
 void loop() {
-  if (button_boot.pressed) { //to connect wifi via Android esp touch app 
+  if (button_boot.pressed) { //to connect wifi via Android esp touch app
     Serial.println("Firmware update Starting..");
     firmwareUpdate();
     button_boot.pressed = false;
   }
   repeatedCall();
 }
+
 
 void connect_wifi() {
   Serial.println("Waiting for WiFi");
@@ -118,19 +120,21 @@ void firmwareUpdate(void) {
   t_httpUpdate_return ret = httpUpdate.update(client, URL_fw_Bin);
 
   switch (ret) {
-  case HTTP_UPDATE_FAILED:
-    Serial.printf("HTTP_UPDATE_FAILD Error (%d): %s\n", httpUpdate.getLastError(), httpUpdate.getLastErrorString().c_str());
-    break;
+    case HTTP_UPDATE_FAILED:
+      Serial.printf("HTTP_UPDATE_FAILD Error (%d): %s\n", httpUpdate.getLastError(), httpUpdate.getLastErrorString().c_str());
+      break;
 
-  case HTTP_UPDATE_NO_UPDATES:
-    Serial.println("HTTP_UPDATE_NO_UPDATES");
-    break;
+    case HTTP_UPDATE_NO_UPDATES:
+      Serial.println("HTTP_UPDATE_NO_UPDATES");
+      break;
 
-  case HTTP_UPDATE_OK:
-    Serial.println("HTTP_UPDATE_OK");
-    break;
+    case HTTP_UPDATE_OK:
+      Serial.println("HTTP_UPDATE_OK");
+      break;
   }
 }
+
+
 int FirmwareVersionCheck(void) {
   String payload;
   int httpCode;
@@ -141,15 +145,15 @@ int FirmwareVersionCheck(void) {
   Serial.println(fwurl);
   WiFiClientSecure * client = new WiFiClientSecure;
 
-  if (client) 
+  if (client)
   {
     client -> setCACert(rootCACertificate);
 
-    // Add a scoping block for HTTPClient https to make sure it is destroyed before WiFiClientSecure *client is 
+    // Add a scoping block for HTTPClient https to make sure it is destroyed before WiFiClientSecure *client is
     HTTPClient https;
 
-    if (https.begin( * client, fwurl)) 
-    { // HTTPS      
+    if (https.begin( * client, fwurl))
+    { // HTTPS
       Serial.print("[HTTPS] GET...\n");
       // start connection and send HTTP header
       delay(100);
@@ -166,19 +170,19 @@ int FirmwareVersionCheck(void) {
     }
     delete client;
   }
-      
+
   if (httpCode == HTTP_CODE_OK) // if version received
   {
     payload.trim();
     if (payload.equals(FirmwareVer)) {
       Serial.printf("\nDevice already on latest firmware version:%s\n", FirmwareVer);
       return 0;
-    } 
+    }
     else  {
       Serial.println(payload);
       Serial.println("New firmware detected");
       return 1;
     }
-  } 
-  return 0;  
+  }
+  return 0;
 }
