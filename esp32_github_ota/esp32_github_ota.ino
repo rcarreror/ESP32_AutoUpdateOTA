a2fab186eb8fc0d5aa8720343d71668815e41c2f
rcarreror <rcarreror@gmail.com>
2021-10-21 22:27:43 +0200

    V1.6

--- esp32_github_ota/esp32_github_ota.ino
+++ esp32_github_ota/esp32_github_ota.ino
@@ -9,7 +9,7 @@ const char * password = "helloworld458";
 
 
 String FirmwareVer = {
-  "1.5"
+  "1.6"
 };
 #define URL_fw_Version   "https://raw.githubusercontent.com/rcarreror/ESP32_AutoUpdateOTA/main/esp32_github_ota/bin_version.txt"
 #define URL_fw_Bin           "https://raw.githubusercontent.com/rcarreror/ESP32_AutoUpdateOTA/main/esp32_github_ota/fw.bin"
